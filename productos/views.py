f64ae2431b1e1395941284d2f38596d79ba45c65
JuanRinaldi <rinaldijuanpablo@hotmail.com>
2023-02-16 15:59:27 -0300

    cargamos las imagenes y editamos las cartas

--- productos/views.py
+++ productos/views.py
@@ -6,13 +6,23 @@ from django.views.generic.detail import DetailView
 class ProductListView(ListView):
     template_name = 'index.html'
     queryset = Product.objects.all().order_by('-id')
-
-    def get_content_data(self, **kwargs):
-        context= super().get_context_data(**kwargs)
-        context['message'] = 'Listado de productos'
-        
-        return context
-    
+   
 class ProductDetailView(DetailView):
     model = Product 
     template_name = 'product.html'
+    
+class ProductSearchListView(ListView):
+    template_name= 'search.html'
+    
+    def get_queryset(self):
+        return Product.objects.filter(tittle__icontains=self.query())
+    
+    def query(self):
+        return self.request.GET.get('q')
+    
+    def get_content_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        context['count'] = context['product_list'].count()
+
+        return context 
